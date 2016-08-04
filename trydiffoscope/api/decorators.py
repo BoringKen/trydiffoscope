5f44d0436d79a6b188bf2e02caab147d83b03bd0
Chris Lamb <lamby@debian.org>
2016-08-04 18:51:38 +0100

    Never cache API views

--- trydiffoscope/api/decorators.py
+++ trydiffoscope/api/decorators.py
@@ -3,10 +3,12 @@ import functools
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.cache import never_cache
 
 class api_method(object):
     def __call__(self, fn):
         @csrf_exempt
+        @never_cache
         @functools.wraps(fn)
         def wrapped(request, *args, **kwargs):
             response = fn(request, *args, **kwargs)
