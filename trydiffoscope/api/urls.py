2bef52bc6fbd816b4ccdcc2f3b43170123a3e3c0
Chris Lamb <lamby@debian.org>
2016-08-13 22:29:54 +0100

    New v3 API; rename url->uri, specify formats, filenames, etc.

--- trydiffoscope/api/urls.py
+++ trydiffoscope/api/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = (
-    url(r'^api/v2/comparison$', views.compare,
+    url(r'^api/v3/comparison$', views.compare,
         name='compare'),
-    url(r'^api/v2/comparison/(?P<slug>\w{12})$', views.comparison,
+    url(r'^api/v3/comparison/(?P<slug>\w{12})$', views.comparison,
         name='comparison'),
 )
--- trydiffoscope/api/views.py
+++ trydiffoscope/api/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_POST
 
@@ -21,14 +23,28 @@ def compare(request):
     instance = form.save()
 
     return 201, {
-        'url': resolve_absolute_url(request, 'api:comparison', instance.slug),
+        'uri': resolve_absolute_url(request, 'api:comparison', instance.slug),
     }
 
 @api_method()
 def comparison(request, slug):
     instance = get_object_or_404(Comparison, slug=slug)
 
-    return 200, {
-        'url': resolve_absolute_url(request, instance),
+    response = {
+        'uri': resolve_absolute_url(request, instance),
         'state': instance.get_state_enum().name,
+
+        'files': {x: {
+            'name': os.path.basename(getattr(instance, 'file_%s' % x).name),
+            'size': getattr(instance, 'file_%s' % x).size,
+        } for x in ('a', 'b')},
+
+        'formats': {x: resolve_absolute_url(
+            request, 'compare:output', instance.slug, y,
+        ) for x, y in (('text', 'txt'), ('html', 'html'))},
+
+        'created': instance.created,
+        'updated': instance.updated,
     }
+
+    return 200, response
