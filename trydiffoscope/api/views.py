56469b4efde619b0f10b5c92f50729ee8deec05b
Chris Lamb <lamby@debian.org>
2016-08-13 22:30:16 +0100

    Recursively sed resolve_absolute_url resolve_absolute_uri

--- trydiffoscope/api/views.py
+++ trydiffoscope/api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_POST
 
 from trydiffoscope.compare.forms import CompareForm
-from trydiffoscope.utils.shortcuts import resolve_absolute_url
+from trydiffoscope.utils.shortcuts import resolve_absolute_uri
 
 from trydiffoscope.compare.models import Comparison
 
@@ -23,7 +23,7 @@ def compare(request):
     instance = form.save()
 
     return 201, {
-        'uri': resolve_absolute_url(request, 'api:comparison', instance.slug),
+        'uri': resolve_absolute_uri(request, 'api:comparison', instance.slug),
     }
 
 @api_method()
@@ -31,7 +31,7 @@ def comparison(request, slug):
     instance = get_object_or_404(Comparison, slug=slug)
 
     response = {
-        'uri': resolve_absolute_url(request, instance),
+        'uri': resolve_absolute_uri(request, instance),
         'state': instance.get_state_enum().name,
 
         'files': {x: {
@@ -39,7 +39,7 @@ def comparison(request, slug):
             'size': getattr(instance, 'file_%s' % x).size,
         } for x in ('a', 'b')},
 
-        'formats': {x: resolve_absolute_url(
+        'formats': {x: resolve_absolute_uri(
             request, 'compare:output', instance.slug, y,
         ) for x, y in (('text', 'txt'), ('html', 'html'))},
 
--- trydiffoscope/utils/shortcuts.py
+++ trydiffoscope/utils/shortcuts.py
@@ -1,4 +1,4 @@
 from django.shortcuts import resolve_url
 
-def resolve_absolute_url(request, *args, **kwargs):
+def resolve_absolute_uri(request, *args, **kwargs):
     return request.build_absolute_uri(resolve_url(*args, **kwargs))
